ca136ce9d664a7fed0eb530e47c11af46c01d223
Your Name <you@example.com>
2020-12-19 13:45:55 +0700

    [GREEN] implement views, urls, html template,   file, form and pass all tests

--- userauth/forms.py
+++ userauth/forms.py
@@ -1,3 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+
+class Registration(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'password1',
+            'password2'
+        ]
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'password1': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Enter Password'}),
+            'password2': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'})
+        }
--- userauth/views.py
+++ userauth/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render, redirect, resolve_url
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-
+from .forms import Registration
 
 # Create your views here.
+
+
+def user_login (request) :
+    if request.user.is_authenticated:
+        return redirect('main:home')
+    form = Registration()
+    if request.method == "POST" :
+        if request.POST.get('Login', False) == 'Login':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('main:home')
+            else:
+                messages.error(request, "Username or Password is incorrect")
+        elif request.POST.get('Sign Up', False) == 'Sign Up':
+                form = Registration(request.POST)
+                if form.is_valid():
+                    form.save()
+                    user = form.cleaned_data.get('username')
+                    messages.success(request, 'Account was created for ' + user)
+                    username = request.POST['username']
+                    password = request.POST['password1']
+                    user = authenticate(request, username = username, password = password)
+                    login(request, user)
+                    return redirect('main:home')
+
+    context = {}
+    return render(request, 'userauth/userauth.html', context)
+
+def user_logout(request):
+    logout(request)
+    return redirect(resolve_url('userauth:login'))
