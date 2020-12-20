bf540fd9f179dcaf566616b462370d8b466a9322
machffud.tra <machffud.thv@gmail.com>
2020-12-21 03:57:14 +0700

    [GREEN] pass and implement test(auth, delete)

--- testi/views.py
+++ testi/views.py
@@ -10,17 +10,25 @@ def tampilan(request):
     return render(request, 'tampilanTesti.html', context)
 
 def testi(request):
-    testi_form = Testimoni_Form(request.POST or None)
-    if request.method == "POST":
-        if testi_form.is_valid():
-            testi_form.save()
+    if request.user.is_authenticated:
+        testi_form = Testimoni_Form(request.POST or None)
+        if request.method == "POST":
+            if testi_form.is_valid():
+                testi_form.save()
 
-        return redirect('testi:tampilan')
-    
-    context = {
-        'testi_form' : testi_form
-    }
-    return render(request, 'isiTesti.html', context)
+            return redirect('testi:tampilan')
+        
+        context = {
+            'testi_form' : testi_form
+        }
+        return render(request, 'isiTesti.html', context)
+    else:
+        return redirect('userauth:login')
+
+def delete(request, delete_id):
+    Testi.objects.filter(id = delete_id).delete()
+    return redirect('testi:tampilan')
+
 
 
 
