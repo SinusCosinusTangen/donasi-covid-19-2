408b47aac3f7fc2fb3d878f29d007b7a3c07b299
James Frederix Rolianto <jamesfrederix16@gmail.com>
2020-12-31 15:35:57 +0700

    [GREEN] Fix bug

--- donasi/urls.py
+++ donasi/urls.py
@@ -1,10 +1,12 @@
 from donasi.views import seeInstitution
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'donasi'
 
 urlpatterns = [
     path('', views.institutionReg, name="institution"),
     path('seeInstitution/', views.seeInstitution, name="seeInstitution"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
